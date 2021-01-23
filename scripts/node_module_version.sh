d895fed5e12bbdbdaf3a4eb06a2c23c25196b195
Kevin Nedelec <kevin.nedelec@gmail.com>
2021-01-23 12:55:09 +0100

    feat(node_module_version): add -l option to distinguish symlinks
    node_module_versio myLib -l will prefix the myLib version with d when installed
    or l when linked

--- scripts/node_module_version.sh
+++ scripts/node_module_version.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 p_root=("./front" ".")
 
@@ -6,7 +6,12 @@ p_root=("./front" ".")
 # if arg, check in {p_root}/node_modules/arg
 # otherwise check in {p_root}/package.json to get the current module version
 
+# use -l as second arg to distinguish if the module is a symlink or a dir
+# this will prefix with "d" for dir or "l" for link
+
 p_name="package.json"
+found_path=
+package=
 
 function echo_version() {
   echo $(cat $1 \
@@ -20,19 +25,29 @@ function echo_version() {
 # if no arg, consider current dir as a module
 if [[ -z $1 ]]; then
   for path in "${p_root[@]}"; do
-    package="$path/$p_name"
-    if [[ -e $package ]]; then
-      echo_version $package
-      exit 0
+    if [[ -e $path ]]; then
+      found_path=$path
+      package="$path/$p_name"
+      break 1
+    fi
+  done
+# else find in node_modules
+else
+  for path in "${p_root[@]}"; do
+    path="$path/node_modules/$1"
+    if [[ -e $path ]]; then
+      found_path=$path
+      package="$path/$p_name"
+      break 1
     fi
   done
 fi
 
+[[ -z $found_path ]] && exit 1
 
-for path in "${p_root[@]}"; do
-  package="$path/node_modules/$1/$p_name"
-  if [[ -e $package ]]; then
-    echo_version $package
-    exit 0
-  fi
-done
+if [[ $2  == '-l' ]]; then
+  [[ -L $found_path ]] && echo -n "l" || echo -n "d"
+fi
+
+echo_version $package
+exit 0
